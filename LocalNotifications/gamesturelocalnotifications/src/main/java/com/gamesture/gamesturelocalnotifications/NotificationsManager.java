896e23e77b3e8fe21a874effbb125f1b03d388d2
Dariusz Furman <dfurman@gamesture.com>
2017-09-20 13:42:23 +0200

    Added whole android studio project

---
+++ LocalNotifications/gamesturelocalnotifications/src/main/java/com/gamesture/gamesturelocalnotifications/NotificationsManager.java
@@ -0,0 +1,13 @@
+package com.gamesture.gamesturelocalnotifications;
+
+/**
+ * Created by dariuszfurman on 20.09.2017.
+ */
+
+public class NotificationsManager
+{
+    public static String GetText()
+    {
+        return "Debug Log Me";
+    }
+}
